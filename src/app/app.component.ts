56a967207263f81bd9f16a3c4bac34b12474c5a5
loris92 <loris.lauri@eng.it>
2019-07-11 09:40:20 +0200

    Get Associated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Contenitore } from './shared/contenitore.model';
 import { ElementService } from './shared/element.service';
 import { MatDialog, MatDialogConfig} from "@angular/material"
 import { InserimentoComponent } from './inserimento/inserimento.component';
+import { AssociatedService } from './container-associated/associated.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,10 +19,12 @@ export class AppComponent {
   @Output() contenitore = new EventEmitter <{id: string, nome: string}> ();
   
   constructor( private service: ElementService,
+               private assService: AssociatedService,
                 public dialog : MatDialog) { }
 
   ngOnInit () {
     this.service.refreshList();
+    this.assService.GetAssociazione();
 }
 
 getIdByList(event) {
