f26dee7391ba9bcc31e42921d5c1b7e4c2b9f040
loris92 <loris.lauri@eng.it>
2019-07-25 12:14:07 +0200

    Comment

--- src/app/component-list/component-list.component.ts
+++ src/app/component-list/component-list.component.ts
@@ -13,31 +13,40 @@ export class ComponentListComponent implements OnInit {
   
 @Output() container = new EventEmitter <{id: string, nome: string, colore : string}> ();
 
-listId=[]; 
+listIdContenitoriAperti=[]; 
 
   constructor(private service:ElementService,
-              private assService: AssociatedService,
               private serviceCont:ContenitoreService,
               private app: AppComponent) { }
 
   ngOnInit() {
+    // Nell'init del component viene fatto il refresh dei contenitori, un metodo del service che carica da server
+    // tutti i contenitori, il subscribe osserva e cattura il segnale emesso al momento di un aggiornamento dei 
+    // contenitori e in caso di nuovo aggiornamento (modifica, inserimento, eliminazione) lancia di nuovo il refresh
+
     this.serviceCont.refreshContenitori();
+
     this.serviceCont.segnaleAggiornamento.subscribe(()=>{
     this.serviceCont.refreshContenitori();
-  });
+    });
 
-  this.app.IdChiusuraSignal.subscribe(sig=>{
-    this.AggiornaChiusura(sig);
-  })
+    // Il subscribe monitora l'emit della propietà IdChiusuraSignal e lancia il metodo AggiornaChiusura al momento
+    // della chiusura di un contenitore
+
+    this.app.IdChiusuraSignal.subscribe(signal=>{
+    this.AggiornaChiusura(signal);
+    })
   }
 
  espandiContenitore (id,nome,colore) {
 
+  // Metodo che emette in output le proprietà del contenitore che si vuole aprire al momento del click
+  // prima esegue un controllo sulla lista dei contenitori aperti per evitare di aprire un contenitore già aperto
+
    let bool = true;
-   for (let index = 0; index < this.listId.length; index++)
+   for (let index = 0; index < this.listIdContenitoriAperti.length; index++)
    {
-     const element = this.listId[index];
-     if (element == id)
+     if (this.listIdContenitoriAperti[index] == id)
      {
         bool=false;
         break;
@@ -45,14 +54,18 @@ listId=[];
     }
    if (bool) {
     this.service.filtraLista(id);
-    this.listId.push(id);
+    this.listIdContenitoriAperti.push(id);
     this.container.emit({ id:id, nome:nome, colore:colore });
     }
   }
 
   AggiornaChiusura(id: number){
-    if(id!=0 && this.listId.includes(id)){
-        this.listId.splice(this.listId.indexOf(id), 1);
+
+    // Metodo che viene lanciato all'emissione del segnale di chiusura di un contenitore, elimina dalla lista 
+    // dei contenitori aperti l'id del contenitore che si sta chiudendo
+
+    if(id!=0 && this.listIdContenitoriAperti.includes(id)){
+        this.listIdContenitoriAperti.splice(this.listIdContenitoriAperti.indexOf(id), 1);
     }
   }
 }
